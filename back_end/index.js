85e22d8b3b941d9ea54b240dd9820f7611468484
aeromel.laure@student.passerellesnumeriques.org <aeromel.laure@student.passerellesnumeriques.org>
2020-04-08 16:24:26 +0800

    initials

---
+++ back_end/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 4000
+var bodyParser = require('body-parser')
+var cors = require('cors')
+var path = require('path')
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json());
+app.use(cors())
+
+// app.use('/files', express.static(path.join(__dirname, 'photos')))
+
+require('./setup/mongo');
+
+// const user = require('./routes/userRoutes'); 
+// const owner = require('./routes/Property_ImageRoutes'); 
+// app.use('/user', user);
+// app.use('/admin', admin);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- back_end/setup/mongo.js
+++ back_end/setup/mongo.js
@@ -3,7 +3,7 @@ const _CONFIG = require("../config/keys")
 console.log("connecting");
 mongoose.Promise = global.Promise;
 
-mongoose.connect(_CONFIG.atlasURI, {
+mongoose.connect(_CONFIG.mongoURI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
